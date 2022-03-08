22574829f7f89dd6627bfc60a10240181e20e40f
ArthurFakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2022-03-08 14:23:29 -0300

    Criando a lógica a ser utilizada pelo Timer

--- timer/src/App.jsx
+++ timer/src/App.jsx
@@ -1,6 +1,54 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [timer, setTimer] = useState(0);
+  const [allow, setAllow] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (allow) {
+      interval = setInterval(() => {
+        setTimer(timer => timer + .1);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [allow])
+
+  function showTime(timer) {
+    if (unity(timer, 1) !== '00') {
+      return `${unity(timer, 1)} hora(s), ${unity(timer, 0)} minuto(s) e ${(timer % 60).toFixed(2)} segundo(s)`
+    } else if (unity(timer, 0) !== '00') {
+      return `${unity(timer, 0)} minuto(s) e ${(timer % 60).toFixed(2)} segundo(s)`
+    } else
+      return `${(timer % 60).toFixed(2)} segundos`;
+  }
+
+  function unity(value, type) {
+    let ans = "";
+    switch (type) {
+      case 0: ans = Math.floor((value / 60) % 60).toString();
+        break;
+      default: ans = Math.floor((value / 3600) % 24).toString();
+    }
+    return ans.replace(/^[0-9]$/gm, '0' + ans);
+  }
+
+  function play() {
+    setAllow(true);
+  }
+
+  function pause() {
+    setAllow(false);
+  }
+
+  function reset() {
+    setTimer(0);
+    pause();
+  }
 
   return (
     <div className="App">
